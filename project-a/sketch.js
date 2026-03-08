e51b4ca6abdcb7971417d262d683afa22c151d93
vz2152 <vz2152@nyu.edu>
2026-03-08 20:37:56 +0800

    Update sketch.js

--- project-a/sketch.js
+++ project-a/sketch.js
@@ -7,7 +7,7 @@ let householdItems = [];
 
 function setup() {
  let canvas= createCanvas(800, 500); 
-  canvas.parent("p5-canvas-container");}
+  canvas.parent("p5-canvas-container");
   angleMode(DEGREES);
   x = width / 2;
   y = height / 2;
